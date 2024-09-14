c62cfcb8cd91588c54b89fab5e726642ee8a8d88
Tommaso Pellegrino <tommaso.pellegrino@linksmt.it>
2024-09-14 19:04:45 +0200

    Simplified Bresenham for line drawing

--- main.cpp
+++ main.cpp
@@ -31,8 +31,24 @@ typedef struct win32_window_dimension
     int height;
 } win32_window_dimension;
 
+typedef enum
+{
+    RED   = 0x00ff0000,
+    GREEN = 0x0000ff00,
+    BLUE  = 0x000000ff,
+    WHITE = 0x00ffffff
+} Color;
+
 global_variable bool running;
 global_variable win32_offscreen_buffer global_back_buffer;
+
+void
+IntSwap(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
 
 win32_window_dimension
 GetWindowDimension(HWND window)
@@ -48,7 +64,7 @@ GetWindowDimension(HWND window)
 }
 
 internal void
-RenderWeirdGradient(win32_offscreen_buffer buffer, int x_offset, int y_offset)
+RenderWeirdGradient(win32_offscreen_buffer buffer, int blue_offset, int green_offset)
 {
     // let's see what optimizer does
     uint8 *row = (uint8 *)buffer.memory;
@@ -57,13 +73,144 @@ RenderWeirdGradient(win32_offscreen_buffer buffer, int x_offset, int y_offset)
         uint32 *pixel = (uint32 *)row;
         for (int x = 0; x < buffer.width; x++)
         {
-            uint8 Blue = (x + x_offset);
-            uint8 Green = (y + y_offset);
+            uint8 Blue = (x + blue_offset);
+            uint8 Green = (y + green_offset);
 
             *pixel++ = ((Green << 8) | Blue);
         }
 
-        row += buffer.pitch;
+        row += 4 * buffer.pitch;
+    }
+}
+
+// Simple but slow implementation of drawing a line
+internal void
+DrawLineSlowly(win32_offscreen_buffer buffer,
+               int x0, int x1,
+               int y0, int y1,
+               Color color)
+{
+    uint32_t *pixel = (uint32_t *)buffer.memory;
+    for (float t = 0.; t < 1.; t += .001)
+    {
+        int x = x0 + (x1 - x0) * t;
+        int y = y0 + (y1 - y0) * t;
+        *(pixel + y * buffer.pitch + x) = color;
+    }
+}
+
+// Draw line which is garanteed to have slope greater than 1
+// or less than -1
+internal void
+DrawLineHigh(win32_offscreen_buffer buffer,
+             int x0, int x1,
+             int y0, int y1,
+             Color color)
+{
+    // f(x, y) = A * x + B * y + C
+    int A = y1 - y0;
+    int B = x0 - x1;
+    int C = (y0 - y1) * x0 + (x1 - x0) * y0;
+
+    uint32_t *pixel;
+    int x = x0;
+    if (x1 > x0)
+    {
+        for (int y = y0; y < y1; y++)
+        {
+            int f = A * x + B * y + C; 
+            if (f < 0)
+            {
+                x++;
+            }
+            pixel = (uint32_t *)buffer.memory + buffer.pitch * y + x;
+            *pixel = WHITE;
+        }
+    }
+    else
+    {
+        for (int y = y0; y < y1; y++)
+        {
+            int f = A * x + B * y + C; 
+            if (f > 0)
+            {
+                x--;
+            }
+            pixel = (uint32_t *)buffer.memory + buffer.pitch * y + x;
+            *pixel = WHITE;
+        }
+    }
+}
+
+// Draw line which is garanteed to have slope between -1 and 1
+internal void
+DrawLineLow(win32_offscreen_buffer buffer,
+            int x0, int x1,
+            int y0, int y1,
+            Color color)
+{
+    // f(x, y) = A * x + B * y + C
+    int A = y1 - y0;
+    int B = x0 - x1;
+    int C = (y0 - y1) * x0 + (x1 - x0) * y0;
+
+    uint32_t *pixel;
+    int y = y0;
+    if (y1 > y0)
+    {
+        for (int x = x0; x < x1; x++)
+        {
+            int f = A * x + B * y + C; 
+            if (f > 0)
+            {
+                y++;
+            }
+            pixel = (uint32_t *)buffer.memory + buffer.pitch * y + x;
+            *pixel = WHITE;
+        }
+    }
+    else
+    {
+        for (int x = x0; x < x1; x++)
+        {
+            int f = A * x + B * y + C; 
+            if (f < 0)
+            {
+                y--;
+            }
+            pixel = (uint32_t *)buffer.memory + buffer.pitch * y + x;
+            *pixel = WHITE;
+        }
+    }
+}
+
+internal void
+DrawLineBresenham(win32_offscreen_buffer buffer,
+                  int x0, int x1,
+                  int y0, int y1,
+                  Color color)
+{
+    if (abs(x1 - x0) > abs(y1 - y0))
+    {
+        if (x1 > x0)
+        {
+            DrawLineLow(buffer, x0, x1, y0, y1, color);
+        }
+        else
+        {
+            DrawLineLow(buffer, x1, x0, y1, y0, color);
+        }
+    }
+    else
+    {
+        if (y1 > y0)
+        {
+            DrawLineHigh(buffer, x0, x1, y0, y1, color);
+        }
+        else
+        {
+            DrawLineHigh(buffer, x1, x0, y1, y0, color);
+        }
     }
 }
 
@@ -78,11 +225,11 @@ Win32ResizeDIBSection(win32_offscreen_buffer *buffer, int width, int height)
     buffer->width = width;
     buffer->height = height;
     buffer->bytes_per_pixel = 4;
-    buffer->pitch = buffer->bytes_per_pixel * buffer->width;
+    buffer->pitch = buffer->width;
 
     buffer->info.bmiHeader.biSize = sizeof(buffer->info.bmiHeader);
     buffer->info.bmiHeader.biWidth = buffer->width;
-    buffer->info.bmiHeader.biHeight = -buffer->height;
+    buffer->info.bmiHeader.biHeight = buffer->height;
     buffer->info.bmiHeader.biPlanes = 1;
     buffer->info.bmiHeader.biBitCount = 32;
     buffer->info.bmiHeader.biCompression = BI_RGB;
@@ -98,12 +245,13 @@ Win32ResizeDIBSection(win32_offscreen_buffer *buffer, int width, int height)
 
 internal void
 Win32DisplayBufferInWindow(HDC device_context,
-                           win32_window_dimension window_dimension,
+                           int window_width,
+                           int window_height,
                            win32_offscreen_buffer buffer)
 {
     StretchDIBits(
         device_context,
-        0, 0, window_dimension.width, window_dimension.height,
+        0, 0, window_width, window_height,
         0, 0, buffer.width, buffer.height,
         buffer.memory,
         &buffer.info,
@@ -149,7 +297,6 @@ MainWindowProc(HWND hwnd,
         {
             OutputDebugStringA("WM_PAINT\n");
 
-            RenderWeirdGradient(global_back_buffer, 0, 0);
             PAINTSTRUCT paint;
             HDC device_context = BeginPaint(hwnd, &paint);
             int x = paint.rcPaint.left;
@@ -160,7 +307,8 @@ MainWindowProc(HWND hwnd,
             win32_window_dimension window_dimension = GetWindowDimension(hwnd);
             Win32DisplayBufferInWindow(
                 device_context,
-                window_dimension,
+                window_dimension.width,
+                window_dimension.height,
                 global_back_buffer
             );
             EndPaint(hwnd, &paint);
@@ -231,12 +379,25 @@ WinMain(HINSTANCE instance,
                     DispatchMessageA(&message);
                 }
 
-                RenderWeirdGradient(global_back_buffer, x_offset, y_offset);
+                // RenderWeirdGradient(global_back_buffer, x_offset, 0);
+                int line_x0 = 10 % global_back_buffer.width;
+                int line_x1 = 100 % global_back_buffer.width;
+                int line_y0 = 500 % global_back_buffer.height;
+                int line_y1 = 10 % global_back_buffer.height;
+                DrawLineBresenham(
+                    global_back_buffer,
+                    line_x0,
+                    line_x1,
+                    line_y0,
+                    line_y1,
+                    WHITE
+                );
                 HDC device_context = GetDC(window);
                 win32_window_dimension window_dimension = GetWindowDimension(window);
                 Win32DisplayBufferInWindow(
                     device_context,
-                    window_dimension,
+                    window_dimension.width,
+                    window_dimension.height,
                     global_back_buffer
                 );
                 ReleaseDC(window, device_context);
--- make.bat
+++ make.bat
@@ -1,11 +1,10 @@
 @echo off
 
-if "%1" == "build" goto build
-if "%1" == "test"  goto test
-if "%1" == "run"   goto run
-if "%1" == "clear" goto clear
+if "%1" == "build"     goto build
+if "%1" == "run"       goto run
+if "%1" == "clear"     goto clear
 
-echo Invalid argument. Use "build", "test", "run", or "clear".
+echo Invalid argument. Use "build", "run", or "clear".
 goto :eof
 
 :build
@@ -13,19 +12,8 @@ goto :eof
     call cl /nologo /EHsc /Zi main.cpp user32.lib gdi32.lib
     goto :eof
 
-:build_with_resource
-    echo Building using resource file
-    call rc resource.rc
-    call cl /c /nologo /EHsc /Zi main.c
-    call cl main.obj resource.res /link /subsystem:windows user32.lib 
-    goto :eof
-
-:test
-    echo TODO
-    goto :eof
-
 :run
-    echo Executing ...
+    echo Running ...
     call main.exe
     goto :eof
 
@@ -35,7 +23,6 @@ goto :eof
     del main.obj
     del main.pdb
     del main.ilk
-    del resource.res
     del vc140.pdb
     goto :eof
 
